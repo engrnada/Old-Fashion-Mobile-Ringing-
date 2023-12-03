86cb66658f01c94e3b878bf2230890dc8fec09bd
Nada Hassan <engrnada@users.noreply.github.com>
2023-12-03 14:08:10 +0200

    Create LCD_Config.h

---
+++ HAL/LCD/LCD_Config.h
@@ -0,0 +1,24 @@
+/*
+ * CLCD_Config.h
+ *
+ *  Created on: Nov 23, 2023
+ *      Author: Nada
+ */
+
+#ifndef CLCD_CONFIG_H_
+#define CLCD_CONFIG_H_
+
+
+#define CLCD_Mode  8
+
+
+#define CLCD_RS    DIO_PIN0
+#define CLCD_Rw    DIO_PIN1
+#define CLCD_En    DIO_PIN2
+
+
+#define CLCD_DataPort        DIO_PORTA
+#define CLCD_ControlPort     DIO_PORTB
+
+
+#endif /* CLCD_CONFIG_H_ */
